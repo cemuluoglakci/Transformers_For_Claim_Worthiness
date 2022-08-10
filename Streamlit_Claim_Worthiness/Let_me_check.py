5477678f15e6bdc722b2c5d091f9c0c84fd954fb
cemuluoglakci <cemuluoglakci@gmail.com>
2022-08-10 16:48:26 +0300

    loading bertweet weights

--- Streamlit_Claim_Worthiness/Let_me_check.py
+++ Streamlit_Claim_Worthiness/Let_me_check.py
@@ -1,7 +1,17 @@
+import sys
+if ".." not in sys.path:
+    sys.path.append('../')
 import streamlit as st
 import openai
 import pandas as pd
 import numpy as np
+import os
+import torch
+import wandb
+
+#Custom modules
+import utils
+from utils import custom_models, early_stopping, worthiness_checker, constants
 
 st.set_page_config(
     page_icon=':shark:',
@@ -9,38 +19,6 @@ st.set_page_config(
     initial_sidebar_state='auto',
     layout="wide",
 )
-BACKGROUND_COLOR = 'white'
-COLOR = 'black'
-def set_page_container_style(
-        max_width: int = 1100, max_width_100_percent: bool = False,
-        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
-        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
-    ):
-        if max_width_100_percent:
-            max_width_str = f'max-width: 100%;'
-        else:
-            max_width_str = f'max-width: {max_width}px;'
-        st.markdown(
-            f'''
-            <style>
-                .appview-container .sidebar-content {{
-                    padding-top: {padding_top}rem;
-                }}
-                .appview-container .main .block-container {{
-                    {max_width_str}
-                    padding-top: {padding_top}rem;
-                    padding-right: {padding_right}rem;
-                    padding-left: {padding_left}rem;
-                    padding-bottom: {padding_bottom}rem;
-                }}
-                .appview-container .main {{
-                    color: {color};
-                    background-color: {background_color};
-                }}
-            </style>
-            ''',
-            unsafe_allow_html=True,
-        )
 
 st.markdown(""" <style>
 header {visibility: hidden;}
@@ -48,35 +26,33 @@ header {visibility: hidden;}
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
-st.markdown("""<style>
-        .appview-container .main .block-container{{
-        max-width: {percentage_width_main}%;
-        padding-top: {1}rem;
-        padding-right: {1}rem;
-        padding-left: {1}rem;
-        padding-bottom: {1}rem;
-    }}
-
-        .uploadedFile {{display: none}}
-        footer {{visibility: hidden;}}
-</style>""", unsafe_allow_html=True)
-
-st.markdown(""" <style>
-.appview-container .main .block-container{{
-        padding-top: {padding_top}rem;    }}
-</style> """, unsafe_allow_html=True)
-
-
-
-
 st.title('Let me check your Check-Worthiness')
 
 openai.api_key = st.secrets["api_key"]
 fine_tuned_model = st.secrets["fine_tuned_model"]
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+parent_dir = os.path.dirname(os.path.abspath(os.getcwd()))
+seed_list = [7, 42] # seed_list = [7, 42, 127]
+fold_count = 3 #5
+patience=5
 
-c30, c31, c32 = st.columns([2,1,2])
-with c31:
-    st.image("./media/pandaai.png", use_column_width=True)
+constants = constants.Constants()
+constants.device = device
+constants.parent_dir = parent_dir
+constants.seed_list = seed_list
+constants.fold_count = fold_count
+constants.patience = patience
+
+api = wandb.Api()
+best_sweep = '2afv0m0i'
+sweep = api.sweep("cemulu/Transformers_For_ClaimWorthiness/" + best_sweep)
+best_run = sweep.best_run()
+best_run.summary.get("avg_val_mAP")
+worthiness_checker = worthiness_checker.WorthinessChecker(best_run, constants)
+
+model_file_name = 'vinai_bertweet-covid19-base-uncased_0.7651173954688729.pt'
+PATH = os.path.join(parent_dir, 'Model', model_file_name)
+worthiness_checker.load_model(PATH)
 
 def check_worthiness(tweet):
     tweet = tweet + "\n\n###\n\n"
@@ -85,10 +61,16 @@ def check_worthiness(tweet):
     return result
 
 
-input = st.text_input('Input:')
+input = st.text_input("",placeholder='Is this statement Chech-Worthy?')
 if st.button('Submit'):
     st.write('**Output**')
     st.write(f"""---""")
     with st.spinner(text='In progress'):
         report_text = check_worthiness(input)
-        st.markdown(report_text)
+        st.markdown('GPT3 says: '+report_text)
+    with st.spinner(text='In progress'):
+        probability = worthiness_checker.predict(input)
+        st.markdown('BERTweet says: ' + str(probability))
+c30, c31, c32, c33, c34 = st.columns([1,1,2,1,1])
+with c32:
+    st.image("./media/pandaai.png", use_column_width=True)
